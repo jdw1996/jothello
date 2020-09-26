e143edb0599a5cff0225f98fb171f3dbae2f34fe
Joseph Daniel Winters <jdwinters96@gmail.com>
2020-09-26 18:37:29 -0400

    Updates

---
+++ precache-manifest.ca01c2af408814dc906dc4f9590a731f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568bd9c0e80fb829dc316dec827e8a85",
+    "url": "/jothello/index.html"
+  },
+  {
+    "revision": "ffd4778dc4c9c84abb85",
+    "url": "/jothello/static/css/main.43d136e4.chunk.css"
+  },
+  {
+    "revision": "36fb841a61c7632c7214",
+    "url": "/jothello/static/js/2.f9188d19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jothello/static/js/2.f9188d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd4778dc4c9c84abb85",
+    "url": "/jothello/static/js/main.a4564501.chunk.js"
+  },
+  {
+    "revision": "e850661392a14b55faff",
+    "url": "/jothello/static/js/runtime-main.6151fe91.js"
+  }
+]);
