86fcc3658771a6b27c7232690d32634fc92c04d0
Joseph Daniel Winters <jdwinters96@gmail.com>
2020-09-30 21:00:31 -0400

    Formatting improvements

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ function markerToStr(marker: Marker): string {
   }
 }
 
-function createBoardArray(x: number, y: number) : Marker[][] {
+function createBoardArray(x: number, y: number): Marker[][] {
   let ret = []
   for (let i = 0; i < y; ++i) {
     let row = Array(x).fill(Marker.FREE)
@@ -27,16 +27,16 @@ function createBoardArray(x: number, y: number) : Marker[][] {
   const halfX = Math.floor(x / 2)
   const halfY = Math.floor(y / 2)
   ret[halfY][halfX] = Marker.BOT
-  ret[halfY][halfX-1] = Marker.HUMAN
-  ret[halfY-1][halfX] = Marker.HUMAN
-  ret[halfY-1][halfX-1] = Marker.BOT
+  ret[halfY][halfX - 1] = Marker.HUMAN
+  ret[halfY - 1][halfX] = Marker.HUMAN
+  ret[halfY - 1][halfX - 1] = Marker.BOT
   return ret
 }
 
 function flippableInDirection(
   board: Marker[][], x: number, y: number, i: number, j: number, player: Marker
-) : [number,number][] {
-  let flippable: [number,number][] = []
+): [number, number][] {
+  let flippable: [number, number][] = []
   while (true) {
     x += i
     y += j
@@ -46,14 +46,14 @@ function flippableInDirection(
     } else if (board[y][x] === player) {
       return flippable
     } else {
-      flippable.push([x,y])
+      flippable.push([x, y])
     }
   }
 }
 
-function flippablePositions(board: Marker[][], x: number, y: number, player: Marker) : [number,number][] {
+function flippablePositions(board: Marker[][], x: number, y: number, player: Marker): [number, number][] {
   if (board[y][x] !== Marker.FREE) { return [] }
-  let flippable: [number,number][] = []
+  let flippable: [number, number][] = []
   for (let i = -1; i <= 1; ++i) {
     for (let j = -1; j <= 1; ++j) {
       flippable = flippable.concat(flippableInDirection(board, x, y, i, j, player))
@@ -69,12 +69,12 @@ interface SquareProps {
   onClick: (x: number, y: number) => void
 }
 
-function Square(props: SquareProps) : JSX.Element {
+function Square(props: SquareProps): JSX.Element {
   return (
-      <td className="square" onClick={() => props.onClick(props.x, props.y) }>
-        {markerToStr(props.value)}
-      </td>
-    )
+    <td className="square" onClick={() => props.onClick(props.x, props.y)}>
+      {markerToStr(props.value)}
+    </td>
+  )
 }
 
 interface BoardProps {
@@ -82,7 +82,7 @@ interface BoardProps {
   handleBoardClick: (x: number, y: number) => void
 }
 
-function Board(props: BoardProps) : JSX.Element {
+function Board(props: BoardProps): JSX.Element {
   const handleSquareClick = (x: number, y: number) => { props.handleBoardClick(x, y) }
   return (
     <div>
@@ -99,7 +99,7 @@ function Board(props: BoardProps) : JSX.Element {
   )
 }
 
-function Game() : JSX.Element {
+function Game(): JSX.Element {
   const BOARD_HEIGHT = 8
   const BOARD_WIDTH = 8
   const [isHumanNext, setIsHumanNext] = useState(true)
@@ -134,7 +134,7 @@ function Game() : JSX.Element {
     setIsHumanNext(!isHumanNext)
     const boardArrayClone = boardArray.slice()
     boardArrayClone[y][x] = nextPlayer
-    validMoves.get(currentKey)?.forEach(([i,j]) => {
+    validMoves.get(currentKey)?.forEach(([i, j]) => {
       boardArrayClone[j][i] = nextPlayer
     });
     setBoardArray(boardArrayClone)
